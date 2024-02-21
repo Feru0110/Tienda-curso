628ae10809df617dfb66fa73d6e63b52a154aac4
Feru0110 <mugalde70964@ufide.ac.cr>
2024-02-21 14:01:50 -0600

    Semana 6
    cambios semana 6

---
+++ Tienda/src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,23 @@
+
+package com.tienda.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-v1-2e71a.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "tienda";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-v1-2e71a-firebase-adminsdk-jfatk-2f5d8ed679.json";
+}
